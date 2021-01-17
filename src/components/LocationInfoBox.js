16dd81e1d5eb5e7ec34a290d0eba45ff9f0ccb43
Adam Tern <adater27@gmail.com>
2021-01-17 13:15:14 +0100

    commit

--- src/components/LocationInfoBox.js
+++ src/components/LocationInfoBox.js
@@ -5,7 +5,7 @@ const LocationInfoBox = ({ info }) => {
             <h2>Event Location Info</h2>
             <ul>
     <li>ID: <strong>{ info.id }</strong></li>
-    <li>TITLE: <strong>{ info.title }</strong></li>
+    <li>TITLE : <strong>{ info.title }</strong></li>
             </ul>
         </div>
     )
